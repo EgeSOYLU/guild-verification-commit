4fd08670b914872ccda4984daa10a1387c4a0423
ege soylu <alicnc2626@gmail.com>
2026-04-09 22:29:41 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-08T18:49:14.064Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-04-09T19:29:39.698Z
+Commit: Fix broken links in documentation
