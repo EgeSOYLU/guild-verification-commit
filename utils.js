d30b79f8d4c8c9864827ee36e5c0f7aee67fa1a2
ege soylu <alicnc2626@gmail.com>
2026-04-27 19:51:22 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-21T19:18:27.361Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-27T16:51:21.779Z
+Commit: Fix race condition in async operations
