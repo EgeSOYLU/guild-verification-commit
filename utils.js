0d74c37c4f3b3b0b91694470593a573efda73c5a
ege soylu <alicnc2626@gmail.com>
2026-04-28 18:53:14 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-27T16:51:21.779Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-04-28T15:53:13.895Z
+Commit: Fix database connection timeout
