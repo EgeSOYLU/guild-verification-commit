6aa300544eb21475295045863f30d9b90ad70a5b
ege soylu <alicnc2626@gmail.com>
2026-04-29 04:13:39 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-25T03:27:23.327Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-29T01:13:38.469Z
+Commit: Resolve null pointer exception in user service
